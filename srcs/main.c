94516c72d8aa3f97963c9911b983f18647d4964f
markoscomks <markoscomks@gmail.com>
2024-09-25 15:35:41 +0100

    New folder

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,19 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 22:10:17 by marsoare          #+#    #+#             */
-/*   Updated: 2024/09/25 13:15:43 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/09/25 15:31:35 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int main() {
+int main()
+{
 	char	*input;
 
 	while (1)
 	{
-		input = readline(RED"HELLSH🔥> "DEFAULT); 
+		input = readline(B_RED PROMPT DEFAULT);
 		if (input == NULL || !ft_strcmp(input, "exit"))
 		{
 			printf("\nExiting shell...\n");
