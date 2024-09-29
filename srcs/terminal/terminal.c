47245a04a3e2b419953160e14c56fc22359ba238
markoscomks <markoscomks@gmail.com>
2024-09-29 16:48:21 +0100

    Added newer lexer function and shell structure

--- srcs/terminal/terminal.c
+++ srcs/terminal/terminal.c
@@ -6,13 +6,13 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 15:38:46 by marsoare          #+#    #+#             */
-/*   Updated: 2024/09/28 20:26:14 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/09/29 16:43:47 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-void	terminal(void)
+void	terminal(t_shell *shell)
 {
 	char	*input;
 
@@ -20,6 +20,7 @@ void	terminal(void)
 	{
 		input = readline(B_RED PROMPT DEFAULT);
 		input_validation(input);
+		lexer(shell);
 		if (input == NULL || !ft_strcmp(input, "exit"))
 		{
 			printf("\nExiting shell...\n");
