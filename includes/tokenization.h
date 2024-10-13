cf0659b6c13d1ad3fd451f501d8a4353a50b4710
markoscomks <markoscomks@gmail.com>
2024-10-13 15:43:44 +0100

    Added new tokenization method without split

--- includes/tokenization.h
+++ includes/tokenization.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:41:18 by marsoare          #+#    #+#             */
-/*   Updated: 2024/09/29 16:43:06 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/13 15:14:34 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
 
 //lexer.c
 void	lexer(t_shell *shell, char	*input);
+t_list	*tokenize_input(const char *input);
+int	handle_redir(t_list **tokens, const char *input, int i);
+int	handle_pipe(t_list **tokens, const char *input, int i);
+int handle_quotes(t_list **tokens, const char *input, int i);
 
 #endif
--- srcs/tokenization/lexer.c
+++ srcs/tokenization/lexer.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:38:23 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/01 17:11:56 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/13 14:54:03 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,8 @@
 
 void	lexer(t_shell *shell, char	*input)
 {
-	int	i;
-
-	i = 0;
 	shell->token_lst = NULL;
 	input = ft_strtrim(input, "\t ");
-	shell->input = ft_split_dlim(input, METACHARS);
-	while (shell->input[i])
-	{
-		ft_lstadd_back(&shell->token_lst, ft_lstnew(shell->input[i]));
-		i++;
-	}
+	shell->token_lst = tokenize_input(input);
 	free(input);
 }
