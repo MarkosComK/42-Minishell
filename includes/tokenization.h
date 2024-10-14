1b58257f760c09df8272c9f17455a304df161497
markoscomks <markoscomks@gmail.com>
2024-10-14 13:26:06 +0100

    Added tokens structs for organize and tokenize

--- includes/tokenization.h
+++ includes/tokenization.h
@@ -15,11 +15,37 @@
 
 #include <structs.h>
 
-//lexer.c
-void	lexer(t_shell *shell, char	*input);
+typedef enum s_token_type
+{
+	WORD,
+	PIPE,
+	APPEND,
+	OUTFILE,
+	INFILE,
+}	t_token_type;
+
+typedef enum s_token_state
+{
+	GENERAL,
+	IN_DQUOTES,
+	IN_SQUOTES
+}	t_token_state;
+
+typedef struct s_token
+{
+	char			*content;
+	t_token_type	type;
+	t_token_state	state;
+	int				pos;
+}	t_token;
+
+//create_token_lst.c
 t_list	*tokenize_input(const char *input);
 int	handle_redir(t_list **tokens, const char *input, int i);
 int	handle_pipe(t_list **tokens, const char *input, int i);
 int handle_quotes(t_list **tokens, const char *input, int i);
+int	handle_word_token(t_list **tokens, const char *input, int i);
+//lexer.c
+void	lexer(t_shell *shell, char	*input);
 
 #endif
