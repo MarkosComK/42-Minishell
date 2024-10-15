1e21d5deb13c03d833e4290878158941b5b6000e
markoscomks <markoscomks@gmail.com>
2024-10-15 11:53:28 +0100

    Added well formated printf to check all tokens in the list

--- srcs/utils/printers.c
+++ srcs/utils/printers.c
@@ -24,15 +24,43 @@ void	print_lst(t_list *lst)
 
 void	print_token_lst(t_list *lst)
 {
-	t_token *token;
+	t_token	*token;
 
+	printf("+-----------------+------------+--------------+--------+\n");
+	printf("|     Value       |    Type    |    State     |  Pos   |\n");
+	printf("+-----------------+------------+--------------+--------+\n");
 	while (lst)
 	{
 		token = (t_token *)lst->content;
-		printf("[%s]➜", token->value);
+		printf("| %-15s | ", token->value);
+
+		// Print the type
+		if (token->type == WORD)
+			printf("%-10s | ", "WORD");
+		else if (token->type == PIPE)
+			printf("%-10s | ", "PIPE");
+		else if (token->type == APPEND)
+			printf("%-10s | ", "APPEND");
+		else if (token->type == OUTFILE)
+			printf("%-10s | ", "OUTFILE");
+		else if (token->type == INFILE)
+			printf("%-10s | ", "INFILE");
+
+		// Print the state
+		if (token->state == GENERAL)
+			printf("%-12s | ", "GENERAL");
+		else if (token->state == IN_DQUOTES)
+			printf("%-12s | ", "IN_DQUOTES");
+		else if (token->state == IN_SQUOTES)
+			printf("%-12s | ", "IN_SQUOTES");
+
+		// Print the position
+		printf("%-6d |\n", token->pos);
+
+		// Move to the next token in the list
 		lst = lst->next;
 	}
-	printf("0\n");
+	printf("+-----------------+------------+--------------+--------+\n");
 }
 
 void	print_cmd_lst(t_shell *shell)
