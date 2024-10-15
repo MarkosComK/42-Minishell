c600651973ef598be5c9ceca28d3c9cb619e6bfc
markoscomks <markoscomks@gmail.com>
2024-10-15 17:38:49 +0100

    Created new file
    added function to set token pos

---
+++ srcs/tokenization/set_token_pos.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_token_pos.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 16:39:04 by marsoare          #+#    #+#             */
+/*   Updated: 2024/10/15 16:43:09 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	set_token_pos(t_list *lst)
+{
+	t_token *token;
+	int		i;
+
+	i = 0;
+	while (lst)
+	{
+		token = (t_token *) lst->content;
+		token->pos = i;
+		lst = lst->next;
+		i++;
+	}
+}
