66cdbd1411014e38951d2c4ea1028e50176ef67c
markoscomks <markoscomks@gmail.com>
2024-10-16 12:16:56 +0100

    Refactored print function for more readability

--- srcs/binary_tree/bst_utls.c
+++ srcs/binary_tree/bst_utls.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 08:54:20 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/16 11:24:36 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:16:21 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void print_bst_pipe(t_pipe *node, int space)
 	printf("\n");
 	for (int i = 5; i < space; i++)
 		printf(" ");
-	printf("[%c]\n", node->type.type);
+	if (node->type.type == N_PIPE)
+	printf("[PIPE]\n");
 }
 
 void print_bst_exec(t_exec *node, int space)
@@ -28,25 +29,30 @@ void print_bst_exec(t_exec *node, int space)
 	printf("%s\n", node->command);
 }
 
-void	*print_bst(void *root, int space)
+void print_pipe_children(t_pipe *pipe, int space)
 {
-	int		spacing;
-	t_node	*node;
-	//t_pipe		*pipe;
-	//t_exec		*exec;
+	if (pipe->right)
+		print_bst(pipe->right, space);
+	print_bst_pipe(pipe, space);
+	if (pipe->left)
+		print_bst(pipe->left, space);
+}
 
+void *print_bst(void *root, int space)
+{
+	t_node *node;
+	int		spacing;
+
+	if (space == 5)
+		printf("\n\n\nTREE VIEW:\n");
 	spacing = 5;
 	space += spacing;
 	if (!root)
 		return NULL;
 	node = (t_node *)root;
-	if (node->type == N_PIPE && ((t_pipe *)root)->right)
-		print_bst((t_exec *)((t_pipe *)root)->right, space);
-	if (node->type == N_PIPE && ((t_pipe *)root)->right && ((t_pipe *)root)->left)
-		print_bst_pipe((t_pipe *)root, space);
-	if (node->type == N_PIPE && ((t_pipe *)root)->left)
-		print_bst((t_exec *)((t_pipe *)root)->left, space);
-	if (node->type != N_PIPE)
+	if (node->type == N_PIPE)
+		print_pipe_children((t_pipe *)root, space);
+	else
 		print_bst_exec((t_exec *)root, space);
 	return NULL;
 }
