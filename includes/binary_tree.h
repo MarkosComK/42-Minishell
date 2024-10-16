af99bfdb99b4035f3bca244404009a307e849428
markoscomks <markoscomks@gmail.com>
2024-10-16 12:37:25 +0100

    changed file name

--- includes/binary_tree.h
+++ includes/binary_tree.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 18:53:24 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/16 12:32:15 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:37:22 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ typedef struct	s_exec
 	char		*command;
 }				t_exec;
 
-//bst_utils.c
+//bst_print.c
 void	*print_bst(void *root, int space);
 void	print_pipe_children(t_pipe *pipe, int space);
 void	print_bst_pipe(t_pipe *node, int space);
