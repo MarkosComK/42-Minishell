641b0dbdb32d7b84885d87d2cbde3e75d7459f32
MarkosComK <markoscomks@gmail.com>
2024-10-20 17:44:59 +0100

    Created: function to find path

--- srcs/environment/find_cmd_path.c
+++ srcs/environment/find_cmd_path.c
@@ -6,41 +6,49 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:18:22 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/20 17:18:30 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:34:04 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char *find_command_path(t_list *path_list, const char *command) {
-    char *full_path;
+char *find_cmd_path(t_list *path_list, const char *command)
+{
+    char *cmd_path;
+    char *only_path;
     t_list *current;
-    size_t len;
 
     // Iterate through the linked list of PATH directories
     current = path_list;
-    while (current != NULL) {
+    while (current != NULL)
+    {
         char *path_dir = (char *)current->content;
-        
-        // Allocate memory for the full path (directory + "/" + command)
-        len = strlen(path_dir) + strlen(command) + 2; // +2 for '/' and null terminator
-        full_path = (char *)malloc(len);
-        if (!full_path) {
-            perror("malloc");
+
+        // Use ft_strjoin to concatenate the path and command
+        only_path = ft_strjoin(path_dir, "/");
+        if (!only_path)
+        {
+            perror("ft_strjoin");
             return NULL;
         }
 
-        // Build the full path string (directory + "/" + command)
-        snprintf(full_path, len, "%s/%s", path_dir, command);
+        cmd_path = ft_strjoin(only_path, command); // Combine with the command
+        free(only_path);  // Free the intermediate result after use
 
-        // Check if the file exists and is executable
-        if (access(full_path, X_OK) == 0) {
-            return full_path;  // Command found, return the full path
+        if (!cmd_path)
+        {
+            perror("ft_strjoin");
+            return NULL;
         }
 
-        // Free the allocated memory for the full path if not found
-        free(full_path);
-        current = current->next;
+        // Check if the file exists and is executable
+        if (access(cmd_path, F_OK | X_OK) == 0)
+        {
+            return cmd_path;  // Command found, return the full path
+        }
+
+        free(cmd_path);  // Free the allocated memory if command not found
+        current = current->next;  // Move to the next directory in the list
     }
 
     // If the command was not found in any directory, return NULL
