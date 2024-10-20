2d9630d13e63e0b0615660d76afdcfee8e90703c
MarkosComK <markoscomks@gmail.com>
2024-10-20 17:44:33 +0100

    Changed: exec now receive shell struct

--- includes/exec.h
+++ includes/exec.h
@@ -6,13 +6,15 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 12:42:37 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/19 12:45:34 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:36:23 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
-void	exec_tree(void *root);
+# include <structs.h>
+
+void	exec_tree(t_shell *shell, void *root);
 
 #endif
