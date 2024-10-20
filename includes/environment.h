236cfd50744208aef39076f4e13ab13b35c6bf17
MarkosComK <markoscomks@gmail.com>
2024-10-20 17:20:50 +0100

    Change: structure for readability

--- includes/environment.h
+++ includes/environment.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 14:46:02 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/20 17:00:14 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/20 17:20:11 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 
 # include <structs.h>
 
+//create_path_list.c
 t_list	*path_list(char **envp);
+int	get_path(t_list	**path_list, char *path, int i);
+//environment_printers.c
 void	print_env_lst(t_list *lst);
+//find_command_path.c
+char *find_command_path(t_list *path_list, const char *command);
 
 #endif
