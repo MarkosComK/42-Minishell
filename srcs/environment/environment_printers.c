789a7b0e8f1b7fb387a15a0ad3dc92e9ec0dbe59
MarkosComK <markoscomks@gmail.com>
2024-10-20 17:01:10 +0100

    Added: printer for environment list

--- srcs/environment/environment_printers.c
+++ srcs/environment/environment_printers.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   environment.h                                      :+:      :+:    :+:   */
+/*   environment_printers.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/20 14:46:02 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/20 17:00:14 by marsoare         ###   ########.fr       */
+/*   Created: 2024/10/20 16:59:15 by marsoare          #+#    #+#             */
+/*   Updated: 2024/10/20 17:00:01 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ENVIRONMENT_H
-# define ENVIRONMENT_H
+#include <minishell.h>
 
-# include <structs.h>
-
-t_list	*path_list(char **envp);
-void	print_env_lst(t_list *lst);
-
-#endif
+void	print_env_lst(t_list *lst)
+{
+	printf("ENV LIST:\n");
+	while (lst)
+	{
+		printf("[%s]➜", (char *)lst->content);
+		lst = lst -> next;
+	}
+	printf("NULL\n");
+}
