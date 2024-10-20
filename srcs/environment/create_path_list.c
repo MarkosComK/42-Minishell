c2efe2e01a892ab27c8a85ef58418d6f52b9727e
MarkosComK <markoscomks@gmail.com>
2024-10-20 14:44:14 +0100

    Basic function to check path

--- srcs/environment/create_path_list.c
+++ srcs/environment/create_path_list.c
@@ -6,7 +6,22 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 14:39:45 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/20 14:39:58 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/20 14:43:34 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell.h>
+
+t_list	*path_list(char **envp)
+{
+	t_list	*path_list;
+	char	*path;
+	(void)	envp;
+
+	path_list = NULL;
+	path = getenv("PATH");
+	printf("%s\n", path);
+
+
+	return (path_list);
+}
