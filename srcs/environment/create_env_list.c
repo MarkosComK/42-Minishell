a36b6bd4800ff55d6aefc6609d83c1139f19c744
MarkosComK <markoscomks@gmail.com>
2024-10-21 13:10:19 +0100

    Fixed: basic env list creation working

--- srcs/environment/create_env_list.c
+++ srcs/environment/create_env_list.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:25:36 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/21 12:54:16 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/21 13:07:25 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 t_list	*env_list(char **envp)
 {
 	t_list	*env_list;
-	int		i;
+	char	*content;
 
 	env_list = NULL;
 	if (!envp || !*envp)
 		return (printf("empty env\n"), NULL);
-	i = 0;
-	while (**envp)
+	while (*envp)
 	{
-		
+		content = ft_strdup(*envp);
+		ft_lstadd_back(&env_list, ft_lstnew(content));
+		(envp)++;
 	}
+	print_env_lst(env_list);
 	return (env_list);
 }
