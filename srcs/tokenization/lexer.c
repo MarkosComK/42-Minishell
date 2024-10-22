5d57dc70eeccb5c1bca6568c4a6439708c212c3e
MarkosComK <markoscomks@gmail.com>
2024-10-22 21:04:00 +0100

    Quotes being treated

--- srcs/tokenization/lexer.c
+++ srcs/tokenization/lexer.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:38:23 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/22 18:38:25 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/22 21:03:20 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,4 @@ void	lexer(t_shell *shell, char	*input)
 {
 	shell->token_lst = NULL;
 	tokenize_input(shell, input);
-	remove_quotes(shell);
 }
