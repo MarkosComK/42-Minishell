898d4497697ab2fe3723e5ca26627b70d49b8275
AlabarHome <hugollmarta@hotmail.com>
2024-10-24 23:49:51 +0100

    add builtins.h

---
+++ includes/builtins.h
@@ -0,0 +1,20 @@
+
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+
+//ECHO
+int		ft_echo(t_exec *exec_node);
+int		echo_flag_n(char *arg);
+void	echo_output(char **args, int idx, int newline);
+
+
+
+
+
+
+
+
+
+
+#endif
