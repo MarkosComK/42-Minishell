0d5040739e21a6bc0981715a178d21143ee168be
MarkosComK <markoscomks@gmail.com>
2024-10-24 02:40:02 +0100

    Function working properly

--- srcs/binary_tree/build_tree_utils.c
+++ srcs/binary_tree/build_tree_utils.c
@@ -45,28 +45,30 @@ char	**get_argv(t_shell *shell, t_list *token_lst)
 char	**get_infiles(t_shell *shell, t_list **token_lst)
 {
 	char	**infiles;
-	int		total;
 	t_list	*current;
-	(void) shell;
+	int		total;
 	
 	current = *token_lst;
-	infiles = NULL;
 	total = 0;
-	while (((t_token *)current->content)->type == INFILE) //get total of infiles
+	while (current && ((t_token *)current->content)->type == INFILE)
 	{
 		total++;
 		current = current->next->next;
 	}
 	infiles = malloc(sizeof(char *) * (total + 1));
+	if (!infiles)
+		exit_failure(shell, "get_infiles");
 	total = 0;
 	current = *token_lst;
-	while (current && ((t_token *)current->content)->type == INFILE) //get total of infiles
+	while (current && ((t_token *)current->content)->type == INFILE)
 	{
 		infiles[total] = ft_strdup(((t_token *)current->next->content)->value);
+		if (!infiles[total])
+			exit_failure(shell, "get_infiles");
 		current = current->next->next;
 		*token_lst = (*token_lst)->next->next;
 		total++;
 	}
-	infiles[total] = 0;
-	return (infiles); //return NULL case theres no infile so we dont execute dup2 with the infile
+	infiles[total] = NULL;
+	return (infiles);
 }
