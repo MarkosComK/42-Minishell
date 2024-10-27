6e7e2e0a7683e82c868e5ba3db4bd9f2b1440f10
hluiz-ma <hugollmarta@hotmail.com>
2024-10-27 18:52:26 +0000

    created pwd and norm

--- srcs/built-ins/echo.c
+++ srcs/built-ins/echo.c
@@ -1,11 +1,21 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 17:03:20 by hluiz-ma          #+#    #+#             */
+/*   Updated: 2024/10/27 18:46:08 by hluiz-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <minishell.h>
 
 int	ft_echo(t_exec *exec_node)
 {
-	bool newline;
-	unsigned int i;
+	bool			newline;
+	unsigned int	i;
 
 	newline = true;
 	i = 1;
@@ -14,41 +24,38 @@ int	ft_echo(t_exec *exec_node)
 		newline = false;
 		i++;
 	}
-
 	echo_output(exec_node->argv, i, newline);
-
-	return 0;
+	return (0);
 }
 
 int	echo_flag_n(char *arg)
 {
-	int i;
+	int	i;
 
 	if (arg[0] != '-')
-		return 0;
+		return (0);
 	i = 1;
 	while (arg[i])
 	{
 		if (arg[i] != 'n')
-			return 0;
+			return (0);
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
-void    echo_output(char **args, int idx, int newline)
+void	echo_output(char **args, int idx, int newline)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = idx;
 	while (args[i])
 	{
-        ft_putstr(args[i]);
+		ft_putstr(args[i]);
 		if (args[i + 1])
 			write(1, " ", 1);
 		i++;
 	}
-
 	if (newline)
 		write(1, "\n", 1);
 }
