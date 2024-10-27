29fa750d58cbb432dbb5577e8f4f6369d0350387
hluiz-ma <hugollmarta@hotmail.com>
2024-10-27 19:01:14 +0000

    fixed error

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,51 +6,51 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 12:22:19 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/27 17:36:58 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2024/10/27 19:00:35 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <binary_tree.h>
-# include <builtins.h>
-# include <colors.h>
-# include <environment.h>
-# include <errno.h>
-# include <errors.h>
-# include <exec.h>
-# include <fcntl.h>
-# include <input.h>
 # include <libft.h>
-# include <limits.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signals.h>
-# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <structs.h>
-# include <sys/stat.h>
-# include <sys/wait.h>
+# include <colors.h>
+# include <stdbool.h>
+# include <input.h>
 # include <terminal.h>
+# include <errors.h>
 # include <tokenization.h>
 # include <utils.h>
+# include <builtins.h>
+# include <binary_tree.h>
+# include <environment.h>
+# include <exec.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <signals.h>
+# include <limits.h>
 
 // MACROS
 # define PROMPT "🔥 HELLSH ➜ "
-# define MINISHELL YELLOW "hellsh"
+# define MINISHELL YELLOW"hellsh"
 
 /*metacharaters
  * A character that, when unquoted, separates words. A metacharacter is a space
  * , tab, newline, or one of the following characters:
  * ‘|’, ‘&’, ‘;’, ‘(’, ‘)’, ‘<’, or ‘>’
- */
+*/
 # define METACHARS "|;<>"
 
-extern int	e_signo;
+extern int e_signo;
 
-// check_args.c
-int			check_args(int argc, char *argv[], char *envp[]);
-int			exit_code(int value);
+//check_args.c
+int	check_args(int argc, char *argv[], char *envp[]);
+int		exit_code(int	value);
 #endif
