5776a2478a81aa9809f6873cc2cdeec211bb394b
MarkosComK <markoscomks@gmail.com>
2024-10-29 21:18:42 +0000

    Norm fixes

--- srcs/tokenization/lexer.c
+++ srcs/tokenization/lexer.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 16:38:23 by marsoare          #+#    #+#             */
-/*   Updated: 2024/10/27 19:00:59 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/10/29 21:18:34 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,4 @@ void	lexer(t_shell *shell, char	*input)
 {
 	shell->token_lst = NULL;
 	tokenize_input(shell, input);
-	//expand_tokens(shell);
 }
