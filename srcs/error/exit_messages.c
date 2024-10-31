7ed3112fc74ae4c41abc9992c38fcfeee6ad94b5
MarkosComK <markoscomks@gmail.com>
2024-10-31 18:44:04 +0000

    Implemented check for file~directory into oufiles

--- srcs/error/exit_messages.c
+++ srcs/error/exit_messages.c
@@ -51,6 +51,7 @@ void	outfile_failure(t_shell *shell, char *file)
 
 	errno = 0;
 	stat(file, &file_stat);
+	is_directory(shell, file);
 	if (S_ISDIR(file_stat.st_mode))
 	{
 		ft_putstr_fd(MINISHELL " " DEFAULT, 2);
