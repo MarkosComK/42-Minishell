52e8a09de79f9ba08f610c6288e0ad83427017b0
MarkosComK <markoscomks@gmail.com>
2024-11-06 17:03:06 +0000

    Fixed norm

--- srcs/binary_tree/build_tree.c
+++ srcs/binary_tree/build_tree.c
@@ -112,11 +112,9 @@ t_list	*get_name(t_list *tkn_lst)
 			continue ;
 		}
 		if (tkn_lst && ((t_token *)tkn_lst->content)->type == WORD)
-		{
 			if (tkn_lst && ((t_token *)tkn_lst->content)->state != EXPAND
 				&& ft_strlen(((t_token *)tkn_lst->content)->value) != 0)
 				return (tkn_lst);
-		}
 		tkn_lst = tkn_lst->next;
 	}
 	return (word);
