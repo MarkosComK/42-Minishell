bec44b518c082a361ee78d8b88d6c8def1dec526
MarkosComK <markoscomks@gmail.com>
2024-11-07 13:34:36 +0000

    Multiple heredoc ~ working

--- srcs/exec/exec_tree.c
+++ srcs/exec/exec_tree.c
@@ -13,78 +13,8 @@
 #include <minishell.h>
 
 
-int	run_heredoc(t_shell *shell, t_inf *infile, int fd)
-{
-	char	*line;
-
-	if (fd < 0)
-		infile_failure(shell, infile->eof);
-	dprintf(2, B_RED"START_HERED\n"DEFAULT);
-	dprintf(2, B_RED"EOF_IS:%s\n"DEFAULT, infile->eof);
-	while (1)
-	{
-		line = readline("> ");
-		if (line == NULL || !ft_strcmp(line, infile->eof))
-		{
-			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-	}
-	return (fd);
-}
-
-void	set_exec(t_shell *shell, t_exec *exec)
-{
-	t_list	*infiles;
-	t_inf	*inf;
-	int		fd;
-
-	infiles = exec->infiles;
-	(void) shell;
-	while (infiles)
-	{
-		inf = (t_inf *)exec->infiles->content;
-		if (inf->type == HERE)
-		{
-			fd = open(inf->eof, O_RDWR | O_CREAT | O_APPEND, 0644);
-			pid_t	pid;
-			int		status = 0;
-			if ((pid = fork()) == 0)
-			{
-				run_heredoc(shell, inf, fd);
-				close(fd);
-				exit(0);
-			}
-			waitpid(pid, &status, 0);
-			close(fd);
-		}
-		infiles = infiles->next;
-	}
-}
-
-void	handle_heredoc(t_shell *shell, void *root);
-
-void	traverse_pipe(t_shell *shell, t_pipe *pipe)
-{
-	if (pipe->left)
-		handle_heredoc(shell, pipe->left);
-	if (pipe->right)
-		handle_heredoc(shell, pipe->right);
-}
-
-void	handle_heredoc(t_shell *shell, void *root)
-{
-	if (((t_node *)root)->type == N_PIPE)
-		traverse_pipe(shell, root);
-	else if (((t_node *)root)->type == N_EXEC)
-		set_exec(shell, root);
-}
-
-
 void	exec_tree(t_shell *shell, void *root)
 {
-		handle_heredoc(shell, root);
 	if (((t_node *)root)->type == N_PIPE)
 		exec_pipe(shell, root);
 	else if (((t_node *)root)->type == N_EXEC)
--- srcs/terminal/terminal.c
+++ srcs/terminal/terminal.c
@@ -12,6 +12,73 @@
 
 #include <minishell.h>
 
+int	run_heredoc(t_shell *shell, t_inf *infile, int fd)
+{
+	char	*line;
+
+	if (fd < 0)
+		infile_failure(shell, infile->eof);
+	dprintf(2, B_RED"START_HERED\n"DEFAULT);
+	dprintf(2, B_RED"EOF_IS:%s\n"DEFAULT, infile->eof);
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL || !ft_strcmp(line, infile->eof))
+		{
+			break ;
+		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+	}
+	return (fd);
+}
+
+void	set_exec(t_shell *shell, t_exec *exec)
+{
+	t_list	*infiles;
+	t_inf	*inf;
+	int		fd;
+
+	infiles = exec->infiles;
+	(void) shell;
+	while (infiles)
+	{
+		inf = (t_inf *)exec->infiles->content;
+		if (inf->type == HERE)
+		{
+			fd = open(inf->eof, O_RDWR | O_CREAT | O_APPEND, 0644);
+			pid_t	pid;
+			int		status = 0;
+			if ((pid = fork()) == 0)
+			{
+				run_heredoc(shell, inf, fd);
+				close(fd);
+				exit(0);
+			}
+			waitpid(pid, &status, 0);
+			close(fd);
+		}
+		infiles = infiles->next;
+	}
+}
+
+void	handle_heredoc(t_shell *shell, void *root);
+
+void	traverse_pipe(t_shell *shell, t_pipe *pipe)
+{
+	if (pipe->left)
+		handle_heredoc(shell, pipe->left);
+	if (pipe->right)
+		handle_heredoc(shell, pipe->right);
+}
+
+void	handle_heredoc(t_shell *shell, void *root)
+{
+	if (((t_node *)root)->type == N_PIPE)
+		traverse_pipe(shell, root);
+	else if (((t_node *)root)->type == N_EXEC)
+		set_exec(shell, root);
+}
 void	terminal(t_shell *shell, char **envp)
 {
 	start_shell(shell);
@@ -37,6 +104,7 @@ void	terminal(t_shell *shell, char **envp)
 	print_token_lst(shell->token_lst);
 	print_bst(shell->root, 5);
 	set_main_signals();
+	handle_heredoc(shell, shell->root);
 	exec_processes(shell);
 	free_shell(shell);
 	terminal(shell, envp);
