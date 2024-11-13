492bf5992b53b774d644b412b97d80072310f39b
AlabarHome <hugollmarta@hotmail.com>
2024-11-13 22:45:32 +0000

    upt wildcard.h

---
+++ includes/wildcards.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcards.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 14:12:02 by hluiz-ma          #+#    #+#             */
+/*   Updated: 2024/11/10 14:12:02 by hluiz-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WILDCARDS_H
+# define WILDCARDS_H
+
+# include <binary_tree.h>
+# include <dirent.h>
+# include <structs.h>
+
+typedef struct dirent	t_dirent;
+
+// wildcard.c
+char					**process_wildcards(char **args);
+char					**expand_wildcard(char *token);
+int						match_pattern(char *pattern, char *str);
+char					**pattern_to_arr(t_list *matches, char *token);
+char					**merge_expansions(char ***expanded, int count);
+
+// wildcard_utils.c
+void					ft_free_arr(char **arr);
+void					ft_free_expansion(char ***expansion, int count);
+int						ft_arrlen(char **arr);
+t_list					*pattern_new(char *str);
+
+// wildcard_validations.c
+t_list					*get_matches(char *token);
+int						has_wildcard(char **argv);
+int						is_valid_wildcard(char *arg);
+void					check_wildcards(t_shell *shell, t_exec *exec_node);
+
+#endif
