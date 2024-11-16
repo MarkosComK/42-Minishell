eee79d71872bbfb79f036b75dc9d1e69c56680fc
MarkosComK <markoscomks@gmail.com>
2024-11-16 11:47:49 +0000

    Reorganized structure

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 12:42:37 by marsoare          #+#    #+#             */
-/*   Updated: 2024/11/09 17:33:13 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/11/16 11:47:36 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,22 @@
 
 # include <structs.h>
 
-//exec_tree
+//exec_expand.c
+
+//exec_tree.c
 void	lexec_tree(t_shell *shell, void *root);
 void	exec_tree(t_shell *shell, void *root);
 void	exec_pipe(t_shell *shell, t_pipe *pipe_node);
 void	exec_node(t_shell *shell, t_exec *exec_node);
 
-//exec_utils
+//exec_utils.c
 char	*find_cmd_path(t_shell *shell, t_list *path_list, char *command);
 
-//pids_utils
+//pids_utils.c
 void	handle_pid1(t_shell *shell, int pipefd[], t_pipe *pipe_node);
 void	handle_pid2(t_shell *shell, int pipefd[], t_pipe *pipe_node);
 
-//redirects
+//redirects.c
 void	check_files_order(t_shell *shell, t_exec *exec_node);
 void	handle_infiles(t_shell *shell, t_list *infiles, int pos);
 void	handle_outfiles(t_shell *shell, t_list *outfiles, int pos);
