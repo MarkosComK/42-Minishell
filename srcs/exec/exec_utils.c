39030c8ba92c0fff882fb972f63690438e4d42df
MarkosComK <markoscomks@gmail.com>
2024-11-16 13:52:34 +0000

    Norm fixes

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 21:01:00 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2024/11/16 12:48:38 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/11/16 13:51:29 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char	*find_cmd_path(t_shell *shell, t_list *path_list, char *command)
 	char	*path_dir;
 
 	current = path_list;
-	if (ft_strcmp(command, "..") == 0)
-		return (ft_strdup(".."));
+	if (ft_strcmp(command, ".") == 0 || ft_strcmp(command, "..") == 0)
+		return (ft_strdup(command));
 	if (!command || ft_strlen(command) == 0 || !ft_strcmp(command, ""))
 		return (ft_strdup(""));
 	while (current != NULL)
