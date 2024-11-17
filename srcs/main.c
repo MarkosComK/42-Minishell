bdcfe727ebc950912eea0a0e8687ef0ef4f2cb31
MarkosComK <markoscomks@gmail.com>
2024-11-17 12:08:26 +0000

    Update shelllevel

--- srcs/main.c
+++ srcs/main.c
@@ -22,12 +22,35 @@
  * with no extra args
  */
 
+void	update_shlvl(t_shell shell)
+{
+	t_list	*tmp;
+	int		value;
+	char	*n_value;
+
+	tmp = shell.envp;
+	while (tmp)
+	{
+		if (ft_strcmp(((t_env *)tmp->content)->value, "SHLVL=") == 0)
+		{
+			value = ft_atoi(((t_env *)tmp->content)->content);
+			value++;
+			n_value = ft_itoa(value);
+			free(((t_env *)tmp->content)->content);
+			((t_env *)tmp->content)->content = n_value;
+		}
+		tmp = tmp->next;
+	}
+	shell.envp = tmp;
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_shell	shell;
 
 	check_args(argc, argv, envp);
 	env_list(&shell, envp);
+	update_shlvl(shell);
 	terminal(&shell, envp);
 	free_env_lst(shell.envp);
 	return (0);
