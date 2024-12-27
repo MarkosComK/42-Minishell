52971fdf920ffbbdd6f99f32b4bd59edbd49c8e4
Markos Santos <msantos@msantos-mac.lo.devexperts.com>
2024-12-27 17:48:25 +0000

    Fixed status code != 0 breaks minishell

--- srcs/terminal/terminal_utils.c
+++ srcs/terminal/terminal_utils.c
@@ -74,8 +74,6 @@ void	exec_processes(t_shell *shell, void *root)
 	status = 0;
 	set_main_signals();
 	handle_heredoc(shell, root);
-	if (exit_code(-1) != 0)
-		return ;
 	if (handle_exec_node(shell, root, &status))
 		return ;
 	if (fork() == 0)
